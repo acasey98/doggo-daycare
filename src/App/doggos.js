d9640ef647507a53e669aff06753833459853165
Austin Casey <thepoast@gmail.com>
2019-06-18 21:03:56 -0500

    dogs

---
+++ src/App/doggos.js
@@ -0,0 +1,39 @@
+const doggos = [
+  {
+    id: 'doggo1',
+    name: 'Brubbi',
+    type: 'smol dog',
+    barkingIntensity: 'very hihg',
+    friendliness: 8,
+  },
+  {
+    id: 'doggo2',
+    name: 'Bobobo-bobobobo',
+    type: 'beeg dog',
+    barkingIntensity: 'ultra',
+    friendliness: 9,
+  },
+  {
+    id: 'doggo3',
+    name: 'Brachus Rex',
+    type: 'evil dog',
+    barkingIntensity: 'zero',
+    friendliness: 1,
+  },
+  {
+    id: 'doggo4',
+    name: 'BOMBABOBLIMPO',
+    type: 'fat dog',
+    barkingIntensity: 'very low but only because he gets tired',
+    friendliness: 10,
+  },
+  {
+    id: 'doggo5',
+    name: 'Chud',
+    type: 'aardvark',
+    barkingIntensity: 'aardvarks cannot bark',
+    friendliness: 11,
+  },
+];
+
+export default doggos;
---
+++ src/App/employees.js
@@ -0,0 +1,34 @@
+const employees = [
+  {
+    id: 'employee1',
+    name: 'Bringus',
+    species: 'Human',
+    insanometerValue: 2,
+  },
+  {
+    id: 'employee2',
+    name: 'Brungo',
+    species: 'Human, probably...?',
+    insanometerValue: 9,
+  },
+  {
+    id: 'employee3',
+    name: 'Brombo',
+    species: 'dog',
+    insanometerValue: 5,
+  },
+  {
+    id: 'employee4',
+    name: 'Bringo',
+    species: 'cyborg',
+    insanometerValue: 1,
+  },
+  {
+    id: 'employee5',
+    name: 'B. Brimpo',
+    species: 'android',
+    insanometerValue: 8,
+  },
+];
+
+export default employees;
